531fd94f65576cc1fa8daf8e05db10b0dc2730cc
Johannes Brauer <brauer@nordakademie.de>
2024-02-17 21:38:30 +0100

    demo

---
+++ out/demo/static_website.js
@@ -0,0 +1,5 @@
+// Compiled by ClojureScript 1.11.132 {:static-fns true, :optimize-constants true, :optimizations :advanced}
+goog.provide('demo.static_website');
+goog.require('cljs.core');
+goog.require('cljs.core.constants');
+console.log("Hello, Github Pages!");
--- target/public/cljs-out/dev/hello_world/core.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.11.132 {:optimizations :none}
-goog.provide('hello_world.core');
-goog.require('cljs.core');
-console.log("Live edit!!!!!");
-
-//# sourceMappingURL=core.js.map
